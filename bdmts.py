06f7073e7699a920f4c7bed2a031258db422233c
Gambikus <45419685+Gambikus@users.noreply.github.com>
2019-03-31 23:40:49 +0300

    Представление

--- bdmts.py
+++ bdmts.py
@@ -92,52 +92,6 @@ class UsersModel:
         self.connection.commit()
 
 
-class NewsModel:
-    def __init__(self, connection):
-        self.connection = connection
-
-    def init_table(self):
-        cursor = self.connection.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS news 
-                            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                             title VARCHAR(100),
-                             content VARCHAR(1000),
-                             user_id INTEGER
-                             )''')
-        cursor.close()
-        self.connection.commit()
-
-    def insert(self, title, content, user_id):
-        cursor = self.connection.cursor()
-        cursor.execute('''INSERT INTO news 
-                          (title, content, user_id) 
-                          VALUES (?,?,?)''', (title, content, str(user_id),))
-        cursor.close()
-        self.connection.commit()
-
-    def get(self, news_id):
-        cursor = self.connection.cursor()
-        cursor.execute("SELECT * FROM news WHERE id = ?", (str(news_id),))
-        row = cursor.fetchone()
-        return row
-
-    def get_all(self, user_id=None):
-        cursor = self.connection.cursor()
-        if user_id:
-            cursor.execute("SELECT * FROM news WHERE user_id = ?",
-                           (str(user_id),))
-        else:
-            cursor.execute("SELECT * FROM news")
-        rows = cursor.fetchall()
-        return rows
-
-    def delete(self, news_id):
-        cursor = self.connection.cursor()
-        cursor.execute('''DELETE FROM news WHERE id = ?''', (str(news_id)))
-        cursor.close()
-        self.connection.commit()
-
-
 class ProductsModel:
     def __init__(self, connection):
         self.connection = connection
@@ -182,8 +136,6 @@ class ProductsModel:
 
 
 dataBase = DB()
-news122 = NewsModel(dataBase.get_connection())
-news122.init_table()
 k = 0
 products_modell = ProductsModel(dataBase.get_connection())
 products_modell.init_table()
